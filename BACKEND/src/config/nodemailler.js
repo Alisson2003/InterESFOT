04dac8aff1cd8fd740bfb7674a42ff6a6730c26c
Alisson2003 <alissobn.viracocha@epn.edu.ec>
2025-06-17 02:31:18 -0500

    backend-doc-api

--- BACKEND/src/config/nodemailler.js
+++ BACKEND/src/config/nodemailler.js
@@ -16,10 +16,10 @@ let transporter = nodemailer.createTransport({
 const sendMailToRegister = (userMail, token) => {
 
     let mailOptions = {
-        from: process.env.USER_MAILTRAP,
+        from: 'admin@esfot.com',
         to: userMail,
         subject: "INTER_ESFOT-💪",
-        html: `<p>Hola, haz clic <a href="${process.env.URL_FRONTEND}confirmar/${token}">aquí</a> para confirmar tu cuenta.</p>
+        html: `<p>Hola, haz clic <a href="${process.env.URL_BACKEND}/confirmar/${token}">aquí</a> para confirmar tu cuenta.</p>
         <hr>
         <footer>La comunidad de la ESFOT te da la más cordial bienvenida.</footer>
         `
@@ -41,7 +41,7 @@ const sendMailToRecoveryPassword = async(userMail,token)=>{
     html: `
     <h1>INTER_ESFOT-💪</h1>
     <hr>
-    <a href=${process.env.URL_FRONTEND}reset/${token}>Clic para reestablecer tu contraseña</a>
+    <a href=${process.env.URL_BACKEND}reset/${token}>Clic para reestablecer tu contraseña</a>
     <hr>
     <footer>La comunidad ESFOTDIANA te da la más cordial bienvenida.</footer>
     `
