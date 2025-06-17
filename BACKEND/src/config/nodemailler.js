6978623b8f987ac539a6d63cd0f3c64faed1955d
Alisson2003 <alisson.viracocha@epn.edu.ec>
2025-06-17 12:07:32 -0500

    Update nodemailler.js

--- BACKEND/src/config/nodemailler.js
+++ BACKEND/src/config/nodemailler.js
@@ -1,4 +1,4 @@
-import nodemailer from "nodemailer"
+/*import nodemailer from "nodemailer"
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -45,6 +45,67 @@ const sendMailToRecoveryPassword = async(userMail,token)=>{
     <a href=${process.env.URL_BACKEND}reset/${token}>Clic para reestablecer tu contraseña</a>
     <hr>
     <footer>La facultad ESFOT te da la más cordial bienvenida.</footer>
+    `
+    });
+    console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
+}
+
+export {
+    sendMailToRegister,
+    sendMailToRecoveryPassword
+} */
+
+import nodemailer from "nodemailer"
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    //host: process.env.HOST_MAILTRAP,
+    //port: process.env.PORT_MAILTRAP,
+    auth: {
+        user: process.env.USER_MAILTRAP,
+        pass: process.env.PASS_MAILTRAP,
+    }
+});
+
+const sendMailToRegister = (userMail, token) => {
+
+    let mailOptions = {
+        from: 'admin@esfot.com',
+        to: userMail,
+        subject: "INTER_ESFOT-💪",
+        html: `<p>¡Registro exitoso! 🎉
+        <br>Gracias por unirte a la comunidad de la ESFOT.</br>
+        <brAhora solo falta un paso: haz clic en Iniciar y prepárate para disfrutar de experiencias mágicas con nosotros.</br>
+        <br><a href="${process.env.URL_FRONTEND}confirmar/${token}">INICIAR</a></br>
+        <footer>
+        ¡Bienvenido/a a bordo! Estamos emocionados de acompañarte en esta aventura. 🌟
+    </footer>
+        `
+    }
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
+        }
+    })
+}
+
+const sendMailToRecoveryPassword = async(userMail,token)=>{
+    let info = await transporter.sendMail({
+    from: 'admin@esfot.com',
+    to: userMail,
+    subject: "Correo para reestablecer tu contraseña",
+    html: `
+    <h1>INTER_ESFOT-💪</h1>
+    <hr>
+    <a href=${process.env.URL_FRONTEND}reset/${token}>Clic para reestablecer tu contraseña</a>
+    <hr>
+    <footer>¡Bienvenido/a a bordo! Estamos emocionados de acompañarte en esta aventura. 🌟</footer>
     `
     });
     console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
