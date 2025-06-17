da1733543895a96771244af8c1e95f9c2514c51c
Alisson2003 <alissobn.viracocha@epn.edu.ec>
2025-06-17 01:29:13 -0500

    backend-doc-api

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -4,33 +4,32 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import routerAdministrador from './routers/administrador_routes.js';
 
-
-
 // Inicializaciones
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
 //module.exports = app;
 
 // Configuraciones 
-app.set('port',process.env.port || 3000);
-app.use(cors())
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
 
 // Middlewares 
-app.use(express.json())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Para poder recibir datos en formato JSON y URL-encoded
 
 // Variables globales
 
 
 // Rutas 
-app.get('/',(req,res)=>{
-    res.send("Server on")
-})
+app.get('/', (req, res) => {
+    res.send("Server on");
+});
 
 // Rutas para administradores
-app.use('/api',routerAdministrador)
+app.use('/api', routerAdministrador);
+
 // Manejo de una ruta que no sea encontrada
-app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
+app.use((req, res) => res.status(404).send("Endpoint no encontrado - 404"));
 
 // Exportar la instancia de express por medio de app
 export default app;
