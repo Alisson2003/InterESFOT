2e644fda8d77daef465017c0307051d11eb8813c
Alisson2003 <alissobn.viracocha@epn.edu.ec>
2025-07-06 19:20:41 -0500

    url frontend

--- BACKEND/src/config/nodemailler.js
+++ BACKEND/src/config/nodemailler.js
@@ -5,8 +5,8 @@ dotenv.config()
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
-    host: process.env.HOST_MAILTRAP,
-    port: process.env.PORT_MAILTRAP,
+    //host: process.env.HOST_MAILTRAP,
+    //port: process.env.PORT_MAILTRAP,
     auth: {
         user: process.env.USER_MAILTRAP,
         pass: process.env.PASS_MAILTRAP,
@@ -19,9 +19,13 @@ const sendMailToRegister = (userMail, token) => {
         from: 'admin@esfot.com',
         to: userMail,
         subject: "INTER_ESFOT-💪",
-        html: `<p>Hola, haz clic <a href="${process.env.URL_BACKEND}confirmar/${token}">aquí</a> para confirmar tu cuenta.</p>
-        <hr>
-        <footer>La comunidad de la ESFOT te da la más cordial bienvenida.</footer>
+        html: `<p>¡Registro exitoso! 🎉
+        <br>Gracias por unirte a la comunidad de la ESFOT.</br>
+        <brAhora solo falta un paso: haz clic en Iniciar y prepárate para disfrutar de experiencias mágicas con nosotros.</br>
+        <br><a href="${process.env.URL_FRONTEND}confirmar/${token}">INICIAR</a></br>
+        <footer>
+        ¡Bienvenido/a a bordo! Estamos emocionados de acompañarte en esta aventura. 🌟
+    </footer>
         `
     }
 
@@ -42,9 +46,9 @@ const sendMailToRecoveryPassword = async(userMail,token)=>{
     html: `
     <h1>INTER_ESFOT-💪</h1>
     <hr>
-    <a href=${process.env.URL_BACKEND}reset/${token}>Clic para reestablecer tu contraseña</a>
+    <a href=${process.env.URL_FRONTEND}reset/${token}>Clic para reestablecer tu contraseña</a>
     <hr>
-    <footer>La facultad ESFOT te da la más cordial bienvenida.</footer>
+    <footer>¡Bienvenido/a a bordo! Estamos emocionados de acompañarte en esta aventura. 🌟</footer>
     `
     });
     console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
