89576ffd821914b16c6d94a432ec137f2d257cb3
Alisson2003 <alissobn.viracocha@epn.edu.ec>
2025-08-05 23:17:36 -0500

    renderE

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -38,9 +38,9 @@ app.get('/', (req, res) => {
 
 // Inicializaciones
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
 })
 
 app.use(fileUpload({
