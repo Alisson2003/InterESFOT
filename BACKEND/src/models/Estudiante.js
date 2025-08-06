275be04cac97d2e41bddb6d84816b34b4ec89aaf
Alisson2003 <alissobn.viracocha@epn.edu.ec>
2025-08-06 06:34:09 -0500

    login

--- BACKEND/src/models/Estudiante.js
+++ BACKEND/src/models/Estudiante.js
@@ -68,6 +68,8 @@ estudianteSchema.methods.encrypPassword = async function(password) {
 
 // Método para verificar si el password ingresado es el mismo de la BDD
 estudianteSchema.methods.matchPassword = async function(password) {
+    console.log("Password ingresado:", password)
+    console.log("Password cifrado en BD:", this.passwordEstudiante)
     return bcrypt.compare(password, this.passwordEstudiante)
 }
 
