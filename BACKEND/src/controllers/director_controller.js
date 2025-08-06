fb6328e86267ffee6e8ee4b5001870a6b6d4466f
Alisson2003 <alissobn.viracocha@epn.edu.ec>
2025-08-06 01:39:42 -0500

    listar

--- BACKEND/src/controllers/director_controller.js
+++ BACKEND/src/controllers/director_controller.js
@@ -60,24 +60,10 @@ const listarDirectores = async (req,res)=>{
 }*/
 
 const listarDirectores = async (req, res) => {
-    try {
-        console.log("🧪 req.directorBDD:", req.administradorBDD); // Esto imprime qué viene en el request
-
-        const directores = await Director.find({ estadoDirector: true })
-        .where('director')
-        .equals(req.directorBDD)
-        .select("-salida -createdAt -updatedAt -__v")
-        .populate('director', '_id nombre apellido');
-
-        res.status(200).json(directores);
-    } catch (error) {
-        console.error("💥 Error en listarDirectores:", error.message);
-        res.status(500).json({
-        msg: "Error interno del servidor",
-        error: error.message
-        });
-    }
-};
+    const directores = await Director.find({ estadoDirector: true }).where('administrador').equals(req.administradorBDD._id)
+    .select("-salida -createdAt -updatedAt -__v");
+    res.status(200).json(directores);
+}
 
 
 
